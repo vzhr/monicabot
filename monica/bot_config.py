8187e237976cbad388f98fddf4cb4aa996715ab8
vzhr <33412980+vzhr@users.noreply.github.com>
2021-02-01 23:13:46 +0800

    Add files via upload

---
+++ monica/bot_config.py
@@ -0,0 +1,11 @@
+from nonebot.default_config import *
+
+from datetime import timedelta
+
+SUPERUSERS = {1206233944}
+COMMAND_START = {'/', ''}
+SESSION_EXPIRE_TIMEOUT = timedelta(minutes=2)
+
+# 服务器和端口
+HOST = '127.0.0.1'
+PORT = 8765
